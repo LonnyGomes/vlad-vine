9d890e5c5fbb4fbd3eafa13d73aca2419db65f44
Lonny Gomes <code@lonnygomes.com>
2025-11-12 23:54:44 -0500

    feat(ng): add NGOptimizedImage lazy loading support

--- apps/bulbutin-ng/src/app/image-card/image-card.ts
+++ apps/bulbutin-ng/src/app/image-card/image-card.ts
@@ -1,11 +1,11 @@
 import { Component, input } from '@angular/core';
-import { DecimalPipe } from '@angular/common';
+import { DecimalPipe, NgOptimizedImage } from '@angular/common';
 import { DateFormatPipe } from '../pipes/date-format.pipe';
 import { ImageResult } from 'parse-photos';
 
 @Component({
   selector: 'app-image-card',
-  imports: [DateFormatPipe, DecimalPipe],
+  imports: [DateFormatPipe, DecimalPipe, NgOptimizedImage],
   templateUrl: './image-card.html',
   styleUrl: './image-card.scss',
 })
