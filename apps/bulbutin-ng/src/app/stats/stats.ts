601919ed0b2184e116dedd902daf9813bfbf57bd
Lonny Gomes <code@lonnygomes.com>
2025-11-19 01:05:43 -0500

    feat(ng): add state and camera model chart to stats view

--- apps/bulbutin-ng/src/app/stats/stats.ts
+++ apps/bulbutin-ng/src/app/stats/stats.ts
@@ -3,11 +3,13 @@ import { ImageFeed } from '../services/image-feed';
 import { StatsCard } from '../stats-card/stats-card';
 import { AltitudeChart } from '../altitude-chart/altitude-chart';
 import { CountryChart } from '../country-chart/country-chart';
+import { ModelChart } from '../model-chart/model-chart';
+import { StateChart } from '../state-chart/state-chart';
 import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-stats',
-  imports: [StatsCard, AltitudeChart, CountryChart, DecimalPipe],
+  imports: [StatsCard, AltitudeChart, CountryChart, ModelChart, StateChart, DecimalPipe],
   templateUrl: './stats.html',
   styleUrl: './stats.scss',
 })
